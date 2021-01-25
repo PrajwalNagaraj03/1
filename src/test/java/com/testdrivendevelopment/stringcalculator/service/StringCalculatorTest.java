4c8b621163394b55ae68868f8e8ae5e9541757a2
Prajwal N <Prajwal.N@nbcuni.com>
2021-01-25 17:39:04 +0530

    Ordering test cases in order to get count correctly

--- src/test/java/com/testdrivendevelopment/stringcalculator/service/StringCalculatorTest.java
+++ src/test/java/com/testdrivendevelopment/stringcalculator/service/StringCalculatorTest.java
@@ -3,31 +3,40 @@ package com.testdrivendevelopment.stringcalculator.service;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
 
+@TestMethodOrder(OrderAnnotation.class)
 class StringCalculatorTest {
 
 	@Test
+	@Order(1)
 	void test() {
 		// StringCalculator calculator = new StringCalculator();
 		assertEquals(3, StringCalculator.add("1,2"));
 	}
 
 	@Test
+	@Order(2)
 	void emptyCheck() {
 		assertEquals(0, StringCalculator.add(" "));
 	}
 
 	@Test
+	@Order(3)
 	void nullCheck() {
 		assertNull(StringCalculator.add(null));
 	}
 
 	@Test
+	@Order(4)
 	void checkOneNumber() {
 		assertEquals(9, StringCalculator.add("9"));
 	}
 
 	@Test
+	@Order(5)
 	void simpleInputs() {
 		assertEquals(6, StringCalculator.add("1,2,3"));
 		// assertEquals(9, StringCalculator.add("//;\\n1,2,3"));
@@ -35,21 +44,25 @@ class StringCalculatorTest {
 	}
 
 	@Test
+	@Order(6)
 	void handleNewLines() {
 		assertEquals(6, StringCalculator.add("1\n2,3"));
 	}
 	
 	@Test 
+	@Order(7)
 	void handleNewDelimeters() {
 		assertEquals(3, StringCalculator.add("//;\\n1;2"));
 	}
 	
 	@Test
+	@Order(8)
 	void customDelimeters() {
 		assertEquals(27, StringCalculator.add("//;\\n1;2;3;5;7;9"));
 	}
 	
 	@Test
+	@Order(9)
 	void negativeNumbers() {
 		try {
 			StringCalculator.add("-1,-3,3");
@@ -59,6 +72,7 @@ class StringCalculatorTest {
 	}
 	
 	@Test
+	@Order(10)
 	void testWithLetters() {
 		try {
 			StringCalculator.add("a,b,c");
@@ -68,6 +82,7 @@ class StringCalculatorTest {
 	}
 	
 	@Test
+	@Order(11)
 	void invokeCountMethod() {
 		StringCalculator.noOfTimesMethodInvoked();
 	}
